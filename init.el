57455b10cda42674be388fdc8de81f1fa50d7de0
martin <beracochea@gmail.com>
2014-11-08 11:45:38 -0200

    ido mode, improved buffer management

--- init.el
+++ init.el
@@ -2,3 +2,9 @@
   (normal-top-level-add-subdirs-to-load-path))
 ;; powerline
 (require 'powerline)
+;; ido mode
+(ido-mode 1)
+;;display them one per line
+(setq ido-separator "\n")
+;; display any item that contains the chars you typed
+(setq ido-enable-flex-matching t)
