a8dadf24cb09c92cdb0d356b6ad51c1021a94c5b
martin <beracochea@gmail.com>
2014-12-09 20:56:09 -0200

    rn comment

--- init.el
+++ init.el
@@ -22,5 +22,5 @@
  )
 ;; jedi
 (add-hook 'python-mode-hook 'jedi:setup)
-(setq jedi:setup-keys t)                      ; optional
-(setq jedi:complete-on-dot t)                 ; optional
+(setq jedi:setup-keys t)
+(setq jedi:complete-on-dot t)
