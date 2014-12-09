f499ff4a7af408c4e55441e54992df348a936cfe
martin <beracochea@gmail.com>
2014-12-09 20:54:41 -0200

    Add jedi.el and mermelade

--- init.el
+++ init.el
@@ -8,3 +8,19 @@
 (setq ido-separator "\n")
 ;; display any item that contains the chars you typed
 (setq ido-enable-flex-matching t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-archives (quote (("marmalade" . "https://marmalade-repo.org/packages/") ("gnu" . "http://elpa.gnu.org/packages/")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+;; jedi
+(add-hook 'python-mode-hook 'jedi:setup)
+(setq jedi:setup-keys t)                      ; optional
+(setq jedi:complete-on-dot t)                 ; optional
