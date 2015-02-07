e52821d18d77a09c2b277d7db051351911eb60cf
martinbc <beracochea@gmail.com>
2015-02-06 22:47:53 -0200

    change temp files store folder

--- init.el
+++ init.el
@@ -1,16 +1,22 @@
 (let ((default-directory "~/.emacs.d/packages/"))
   (normal-top-level-add-subdirs-to-load-path))
+
 ;; powerline
-(require 'powerline)
+;;(require 'powerline)
+
 ;; ido mode
 (ido-mode 1)
+
 ;; display them one per line
 (setq ido-separator "\n")
+
 ;; display any item that contains the chars you typed
 (setq ido-enable-flex-matching t)
+
 ;; jedi mode
 ;; (add-hook 'python-mode-hook 'jedi:setup)
 ;; (setq jedi:complete-on-dot t)
+
 ;; webmode
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
@@ -20,6 +26,7 @@
 (setq web-mode-markup-indent-offset 2)
 (setq web-mode-css-indent-offset 2)
 (setq web-mode-code-indent-offset 2)
+
 ;; custom variables
 (custom-set-variables
  ;; Your init file should contain only one such instance.
@@ -27,3 +34,9 @@
  '(inhibit-startup-screen t)
  '(package-archives (quote (("marmalade" . "https://marmalade-repo.org/packages/") ("gnu" . "http://elpa.gnu.org/packages/")))))
 (custom-set-faces)
+
+;; store back up files on the temp directory
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
